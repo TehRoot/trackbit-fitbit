ae903bc85b70d09fed8648cafced7433911fbd1b
Mark Cardish <rewt@Marks-MacBook-Pro-2.local>
2018-07-13 20:14:48 -0400

    brought up2date with major changes and work on UI

--- app/index.js
+++ app/index.js
@@ -1,34 +1,62 @@
 import { geolocation } from "geolocation";
+import { PositionOptions } from "geolocation";
 import * as messaging from "messaging";
 import document from "document";
+import { memory } from "system";
 
-let geoData1 = document.getElementById("geolocation-data1");
-let geoData2 = document.getElementById("geolocation-data2");
+let startUpScreen = document.getElementById("selection-screen");
+let geoDataScreen = document.getElementById("data-screen");
 
-messaging.peerSocket.onerror = function(err) {
-  console.log("Connection Error: " + err.code);
-}
-function getWatchLocation(){
-  geolocation.getCurrentPosition(locationSuccess, locationError);
-  function locationSuccess(position) {
-    var coordinates = new Array();
-    coordinates.push((position.coords.latitude).toFixed(6));
-    coordinates.push((position.coords.longitude).toFixed(6));
-    geoData1.text = ("Latitude: " +coordinates[0]);
-    geoData2.text = ("Longitude: "+coordinates[1]);
-    if(coordinates.length == 2){
-      if(messaging.peerSocket.readyState === messaging.peerSocket.OPEN){
-        messaging.peerSocket.send(coordinates);
-      }
-    }
+let geoData1 = geoDataScreen.getElementById("geolocation-data1");
+let geoData2 = geoDataScreen.getElementById("geolocation-data2");
+let geoData3 = geoDataScreen.getElementById("geolocation-data3");
+let geoData4 = geoDataScreen.getElementById("geolocation-data4");
+
+let startUpScreenButton = startUpScreen.getElementById("startButton");
+startUpScreen.style.display = "inline";
+let buttonleft = geoDataScreen.getElementById("button-left");
+
+function waitForPrompt(){
+  startUpScreenButton.onclick = function(evt){
+    startUpScreen.style.display = "none";
+    getWatchLocation();
   }
 }
 
-function locationError(error){
-  console.log("Error: " + error.code, "Message: " + error.message)
+buttonleft.onclick = function(evt){
+  if(messaging.peerSocket.readyState === messaging.peerSocket.OPEN){
+    messaging.peerSocket.send("clear");
+  }
 }
 
-messaging.peerSocket.onopen = function() {
-  getWatchLocation();
-  setInterval(getWatchLocation, 5000);
+messaging.peerSocket.onerror = function(err){
+  console.log("Connection Error: " + err.code);
 }
+
+function getWatchLocation(){
+  var positionAccuracy = { enableHighAccuracy: false };
+  var watchID = geolocation.watchPosition(locationSuccess, locationError, positionAccuracy);
+  function locationSuccess(position) {
+    var coordinates = new Array();
+    coordinates.push(position.coords.latitude);
+    coordinates.push(position.coords.longitude);
+    coordinates.push(Date.now());
+    geoData1.text = ("Latitude: " +coordinates[0].toFixed(6));
+    geoData2.text = ("Longitude: "+coordinates[1].toFixed(6));
+    geoData3.text = ("Accuracy [Meters]: " +position.coords.accuracy);
+    geoData4.text = ("Altitude [Meters]: " +position.coords.altitude);
+    
+    if(messaging.peerSocket.readyState === messaging.peerSocket.OPEN && coordinates.length == 3){
+        console.log(memory.js.used);
+        messaging.peerSocket.send(coordinates);
+        coordinates.length = 0;
+    }
+}  
+    function locationError(error){
+      console.log("Error: " + error.code, "Message: " + error.message)
+    }
+}
+
+messaging.peerSocket.onopen = function(){
+  waitForPrompt();
+} 
--- companion/index.js
+++ companion/index.js
@@ -1,9 +1,20 @@
-import * as messaging from "messaging";
+ import * as messaging from "messaging";
 
 messaging.peerSocket.onmessage = function(evt) {
+  var url = "http://localhost:8080/coordinate";
+  //var url = "https://markcardish.ml/coordinate";
+  var url1 = "https://markcardish.ml/clear";
   console.log(evt.data);
-  var url = "http://localhost:8080/coordinate/add";
-  fetch(url, {
+  if(evt.data == "clear"){
+    fetch(url1, {
+      method: 'POST',
+      body: JSON.stringify(""),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    });
+  } else {
+    fetch(url, {
     method: 'POST',
     body: JSON.stringify(evt.data),
     headers:{
@@ -11,3 +22,11 @@ messaging.peerSocket.onmessage = function(evt) {
     }
   });
   }
+ }
+
+function generateQuickIdentHash(str){
+  var buffer = new TextEncoder("utf-8").encode(str);
+  return crypto.subtle.digest("SHA-256", buffer).then(function (hash) {
+    return hex(hash);
+  });
+}
